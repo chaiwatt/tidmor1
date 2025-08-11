3384c511c5e8d33dd55fc8d148fd65c04f3f24ec
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2025-08-11 07:41:01 +0700

    update

--- database/seeders/Rating.php
+++ database/seeders/Rating.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Faker\Factory as Faker;
 
 class Rating extends Seeder
 {
@@ -13,11 +14,12 @@ class Rating extends Seeder
      */
     public function run(): void
     {
+        $faker = Faker::create(); 
         // สร้างข้อมูลจำลอง 20 รายการ
         for ($i = 0; $i < 20; $i++) {
             DB::table('ratings')->insert([
                 'rating' => rand(4, 5), // สุ่มคะแนนระหว่าง 4 ถึง 5
-                'ip_address' => fake()->ipv4(), // สร้าง IP Address จำลอง
+                'ip_address' => $faker->ipv4, // สร้าง IP Address จำลอง
                 'created_at' => now(),
                 'updated_at' => now(),
             ]);
